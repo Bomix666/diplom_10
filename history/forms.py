715df1f3929e3d945bb1afea9429056b5cb6ca18
Bomix666 <127060112+Bomix666@users.noreply.github.com>
2025-06-05 13:30:56 +0300

    gooo

--- history/forms.py
+++ history/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
 from .models import Event
 
 class UserRegistrationForm(forms.ModelForm):
@@ -10,11 +11,46 @@ class UserRegistrationForm(forms.ModelForm):
         model = User
         fields = ('username', 'first_name', 'email')
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control rounded-pill',
+            'placeholder': 'Имя пользователя'
+        })
+        self.fields['first_name'].widget.attrs.update({
+            'class': 'form-control rounded-pill',
+            'placeholder': 'Ваше имя'
+        })
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control rounded-pill',
+            'placeholder': 'Email'
+        })
+        self.fields['password'].widget.attrs.update({
+            'class': 'form-control rounded-pill',
+            'placeholder': 'Пароль'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control rounded-pill',
+            'placeholder': 'Повторите пароль'
+        })
+
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Пароли не совпадают!')
         return cd['password2']
+
+class CustomAuthenticationForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control rounded-pill',
+            'placeholder': 'Имя пользователя'
+        })
+        self.fields['password'].widget.attrs.update({
+            'class': 'form-control rounded-pill',
+            'placeholder': 'Пароль'
+        })
 
 class EventForm(forms.ModelForm):
     class Meta:
--- history/views.py
+++ history/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
+from .forms import UserRegistrationForm, EventForm, CustomAuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegistrationForm, EventForm
@@ -29,13 +29,13 @@ def register(request):
 
 def user_login(request):
     if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
+        form = CustomAuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
             return redirect('index')
     else:
-        form = AuthenticationForm()
+        form = CustomAuthenticationForm()
     return render(request, 'history/login.html', {'form': form})
 
 def user_logout(request):
